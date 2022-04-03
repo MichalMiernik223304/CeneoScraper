b822fbd4ee91b2b1016c14cd08cf8b6ed9ad90a2
MichalMiernik223304 <92749157+MichalMiernik223304@users.noreply.github.com>
2022-04-03 12:59:25 +0200

    Delete scraper.py

--- scraper.py
+++
@@ -1,9 +0,0 @@
-from urllib import response
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://www.ceneo.pl/66893958#tab=reviews"
-response = requests.get(url)
-page_dom = BeautifulSoup(response.text, 'html.parser')
-
-print(page_dom.prettify())
